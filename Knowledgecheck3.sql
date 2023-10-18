1e362f0666eba21ac0092021a84effe7c2fabf46
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-10-18 16:10:45 -0400

    Update Knowledgecheck3.sql
    finished the skill check

--- Knowledgecheck3.sql
+++ Knowledgecheck3.sql
@@ -1,1 +1,13 @@
+--first query
 
+SELECT C.CustomerName
+FROM Customers AS C
+INNER JOIN Orders AS O ON C.CustomerID = O.CustomerID
+WHERE O.OrderID = 10310;
+
+--second query
+
+SELECT S.Address
+FROM Suppliers AS S
+INNER JOIN Products AS P ON S.SupplierID = P.SupplierID
+WHERE P.ProductID = 40;
